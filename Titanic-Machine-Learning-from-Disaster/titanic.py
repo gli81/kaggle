2e393d32c3893387b6ff4af7784da433770f8129
LGZ <liguangzheng998@hotmail.com>
2023-02-27 14:40:49 +0800

    Worked on Titanic

--- Titanic-Machine-Learning-from-Disaster/titanic.py
+++ Titanic-Machine-Learning-from-Disaster/titanic.py
@@ -5,7 +5,9 @@ import numpy as np
 import pandas as pd
 import os
 
-## LOAD DATA
+"""
+LOAD DATA
+"""
 cwd = os.getcwd()
 # print(cwd)
 train_data = pd.read_csv(os.path.join(
@@ -19,7 +21,24 @@ test_data = pd.read_csv(os.path.join(
 combined = [train_data, test_data]
 
 
+"""
+## A LOOK AT DATA
+# print(train_data.columns.values)
+# print(train_data.head())
+## categorical ones?
+## numerical ones? continuous or discrete?
+some points:
+Ticket is a mix of numeric and alphanumeric data types.
+Cabin is alphanumeric.
+## which features may contain errors or typos?
+## which features contain NAs? Cabin, Age, Embarked
+"""
+train_data.info()
+print('=' * 50)
+test_data.info()
+
+
 import random
 import seaborn as sns
 import matplotlib.pyplot as plt
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
